d38e7588132f66250e95fa8b0532700725284bf6
mhkane <mhkane@mit.edu>
2018-04-05 00:27:18 +0100

    New approach for looking up a context after all the words in the corpus are found out

--- code/word2vecf/generate_context_windows.py
+++ code/word2vecf/generate_context_windows.py
@@ -11,7 +11,7 @@ import random
 import numpy as np
 
 
-if '--cui' or '--cui_rel' or '--cui_rel_separate' or '--cui_rel_only_separate' or '--cui_rel_only' or '--cui_only' in sys.argv:
+if '--cui' or '--cui_rel' or '--cui_rel_separate' or '--cui_rel_only_separate' or '--cui_rel_only' or '--cui_only' or '--cui_rel_once' or '--cui_rel_separate_once' in sys.argv:
     from umls.code.interface_umls import cui_lookup
     from umls.code.interface_umls import cui_relation_lookup
 
@@ -118,6 +118,119 @@ def get_relationships_word(word):
             relationships[cui] = get_relationships_cui(cui)
     return relationships
 
+def extract_context_cuis_once(docs, contexts_filename, N=8,rel=False, cui_only = False, separate_rel= False):
+
+    '''
+    goal: do word2vec's preprocessing
+        [x] dynamic context window
+        [x] rare word removal
+        [x] subsampling
+    '''
+    '''
+    goal: do word2vec's preprocessing
+        [x] dynamic context window
+        [x] rare word removal
+        [x] subsampling
+    '''
+
+    n_docs = len(docs)
+    point = int(n_docs / 20.)
+
+    # unigram counts (for filtering)
+    counts = defaultdict(int)
+    print 'unigram counts'
+    for i,toks in enumerate(docs):
+        if i%point == 0:
+            print '\t%.2f%% done (%d/%d)' % (float(i)/n_docs,i,n_docs)
+        for w in toks:
+            counts[w] += 1
+    print
+    Z = sum(counts.values())
+    freq = { w:float(count)/Z for w,count in counts.items() }
+
+    # remove rare words
+    new_docs = []
+    print 'removing rare words'
+    for i,toks in enumerate(docs):
+        if i%point == 0:
+            print '\t%.2f%% done (%d/%d)' % (float(i)/n_docs,i,n_docs)
+        new_toks = [ w for w in toks if counts[w] >= 5 ]
+        new_docs.append(new_toks)
+    docs = new_docs
+    print
+
+    # subsample to remove frequent words
+    subt = 1e-4
+    new_docs = []
+    print 'subsampling'
+    def remove_prob(w):
+        f = freq[w]
+        return (f-subt)/f - math.sqrt(subt/f)
+    for i,toks in enumerate(docs):
+        if i%point == 0:
+            print '\t%.2f%% done (%d/%d)' % (float(i)/n_docs,i,n_docs)
+        new_toks = [ w for w in toks if random.random() <= 1-remove_prob(w)]
+        new_docs.append(new_toks)
+    docs = new_docs
+    print
+
+    # build context pairs
+    W = defaultdict(int)
+    C = defaultdict(int)
+    #docs = docs[:1]
+    print 'building (w,c) pairs'
+    # save context dict
+    with open(contexts_filename, 'w') as f:
+        for i,toks in enumerate(docs):
+            if i%point == 0:
+                print '\t%.2f%% done (%d/%d)' % (float(i)/n_docs,i,n_docs)
+
+            #toks = toks[:10]
+            doc_len = len(toks)
+            for i,w in enumerate(toks):
+                W[w] += 1
+
+                #print i, w
+                n = random.randint(1,N)
+                i_minus_n = max(i-n  , 0)
+                i_plus_n  = min(i+n+1, doc_len)
+                context = toks[i_minus_n:i] + toks[i+1:i_plus_n]
+                for c in context:
+                    # Unusre if I'm supposed to repeatedly count this for each center word
+                    C[c] += 1
+                    print >>f, '%s %s' % (w,c)
+                    #print '\t%s %s' % (w,c)
+        print
+        for w in W.keys():
+            #Step 1 : Build cui context
+            cui_context = get_cui_context(w)
+            if len(cui_context)>0:
+                if rel : 
+                    if separate_rel: #Separate info from CUI and Neighbor
+                        cui_rel_context = []
+                        for cui in cui_context:
+                            C[cui] +=1
+                            print >>f, '%s %s' % (w,cui)
+                            cui_rel_context = [c_r[0] for c_r in cui_relation_lookup(cui)]
+                            for cui_rel in cui_rel_context:
+                                C[cui_rel] +=1
+                                print >>f, '%s %s' % (cui,cui_rel)
+                    else:
+                        # Word gets matched to CUI + Neighboring CUI
+                        cui_rel_context = []
+                        for cui in cui_context:
+                            cui_rel_context+= [c_r[0] for c_r in cui_relation_lookup(cui)]
+                            cui_rel_context = list(set(cui_rel_context))
+                            context +=cui_context
+                            context +=cui_rel_context
+                            for c in context:
+                            # Unusre if I'm supposed to repeatedly count this for each center word
+                                C[c] += 1
+                                print >>f, '%s %s' % (w,c)
+                            #print '\t%s %s' % (w,c)
+
+    return W, C
+    
 
 def extract_context_cuis(docs, contexts_filename, N=8,rel=False, cui_only = False, separate_rel= False):
 
@@ -243,7 +356,7 @@ def main():
         w_vocab = sys.argv[4]
         c_vocab = sys.argv[5]
         context_type = sys.argv[6]
-        assert context_type in ['--word','--cui','--cui_rel', '--cui_rel_separate','--cui_rel_only_separate','--cui_rel_only', '--cui_only']
+        assert context_type in ['--word','--cui','--cui_rel', '--cui_rel_separate','--cui_rel_only_separate','--cui_rel_only', '--cui_only','--cui_rel_once','--cui_rel_separate_once']
     except Exception, e:
         print '\n\tusage: python %s <corpus> <window_size> <contexts_filename> <w_vocab> <c_vocab> <--word|--cui| --cui_rel>\n'%sys.argv[0]
         exit(1)
@@ -268,6 +381,11 @@ def main():
         W, C = extract_context_cuis(doc_toks,contexts_filename, N=window_size, rel=True, cui_only=True)
     elif context_type == '--cui_only':
         W, C = extract_context_cuis(doc_toks,contexts_filename, N=window_size, cui_only=True)
+    elif context_type == '--cui_rel_once':
+        W, C = extract_context_cuis_once(doc_toks,contexts_filename, N=window_size, rel=True)
+    elif context_type == '--cui_rel_separate_once':
+        W, C = extract_context_cuis_once(doc_toks,contexts_filename, N=window_size, rel=True, separate_rel= True)
+
     else:
         print 'unknown context type "%s"' % context_type
         exit(1)
